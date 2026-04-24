e6fbe8e8f02b2d712a26bd4d7fdef695918470e2
kuslapur <bkuslapur@gmail.com>
2026-04-24 06:52:31 +0530

    update

--- main.tf
+++ main.tf
@@ -5,7 +5,7 @@ provider "aws" {
 resource "aws_instance" "Instance_from_jenkins" {
   ami           = "ami-05d2d839d4f73aafb"
   instance_type = "t2.micro"
-  key_name      = "mykeypair"
+  
   
   tags = {
 	Name = "Jenkins_Instance"
